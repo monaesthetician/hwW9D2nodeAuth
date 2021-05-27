c34f92c11719b0ce238be244cb5597d78cb193c8
Mona <emonearth915@gmail.com>
2021-05-26 21:11:29 -0400

    HW week9 day3

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,27 @@
+const User = require('../models/User');
+
+
+// handle errors
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: ''};
+    
+// duplicate error code
+if (err.code === 11000) {
+    errors.email ='that email is already registerd';
+    return errors;
+}
+
+// validation errors
+if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({properties}) => {
+        errors['properties.path'] = properties.message;
+    });
+ }
+
+ return errors; 
+}
+
 module.exports.sigup_get = (req, res) => {
     res.render('signup');
 }
@@ -7,9 +31,22 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.sigup_post= (req, res) => {
-    res.send('new signup');
+    const { email, password } = req.body;
+
+    try {
+      const user = await User.create({ email, password });
+      res.status(201).json(user); 
+    }
+    catch (err) {
+       const errors =  handleErrors(err);
+       res.statur(400).json({ error });
+    }
 }
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
+    const { email, password } = req.body;
+
+
+    console.log(email, password);
     res.send('user login');
 }
